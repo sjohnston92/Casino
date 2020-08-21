cda1512f3b5546f1fe6d95979c9456c56f591fd1
Scott Mohorn <scottmohorn@gmail.com>
2020-08-21 16:45:27 -0600

    trying to get a class to work

--- slots.rb
+++ slots.rb
@@ -1,1 +1,18 @@
 #slots
+
+class reel
+  attr_accessor :symbol
+  
+  def initialize(symbol) 
+    @symbol = ["plum","goose","cards","knife","dog","pikachu"]
+  end
+
+  def spin_reel
+    @symbol.sample()
+  end
+end
+
+reel.spin_reel
+# class Lineup
+
+
