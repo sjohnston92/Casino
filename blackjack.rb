ec8cd9ffd5d9261a7680ea76bf3087db37537541
Jon Roberts <kewlguy@gmail.com>
2020-08-23 13:53:45 -0600

    added value function and show card function

--- blackjack.rb
+++ blackjack.rb
@@ -7,24 +7,45 @@ require_relative "card"
 @dealer_hand = []
 
 def blackjack_start
-@d = Deck.new
-p @d.class
-#assigning value to each cards (easier to do add for these that have suits).
-#need to do prior to shuffle cards as it will change it into array
 
-@d = @d.shuffle_cards
+#Create new Deck
+@d = Deck.new
+
+ #Shuffling Cards
+ @d = @d.shuffle_cards
+
+ #setting value
+ card_value_set(@d)
+p @d    
 
 start_deal
 
 puts "player hands:"
-print @player_hand
+show_card(@player_hand)
 puts
 puts "dealer hands"
-print @dealer_hand
+show_card(@dealer_hand)
 end
 
+def card_value_set(x)
+    #Assigning cards value
+   x.each do |cards|
+       p cards
+       p cards.rank
+       if cards.rank == "J" || cards.rank == "Q" || cards.rank == "K"
+           cards.value = 10
+       elsif cards.rank == ( "A" )
+           cards.value = 11
+       else
+           cards.value = cards.rank.to_i
+       end
+       p cards
+       end
+end
+
+
 def start_deal
-    #Deal cards to player and dealer
+    #Deal cards to player and dealer TODO: refactor this into loop
     deal(@player_hand)
     deal(@dealer_hand)
     deal(@player_hand)
@@ -35,6 +56,14 @@ end
 def deal(x)
     x << @d.shift
 end
+
+def show_card(x)
+    x.each do |cards|
+        print "[#{cards.rank} #{cards.suit}]"
+    end
+    puts 
+end
+
 # dealer deal a card to player
 # dealer deal a card to dealer (may need to make it hidden)
 
