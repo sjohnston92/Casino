346338d4d3ed103f78f9cdf6dc60abef8e5903c7
Jon Roberts <kewlguy@gmail.com>
2020-08-23 14:45:20 -0600

    Player Turn codes added

--- blackjack.rb
+++ blackjack.rb
@@ -5,8 +5,11 @@ require_relative "deck"
 require_relative "card"
 @player_hand = []
 @dealer_hand = []
+@player_bank = 1000
 
 def blackjack_start
+
+ #need to change this to pull via game menu
 
 #Create new Deck
 @d = Deck.new
@@ -37,7 +40,36 @@ end
 def player_turn
 # sum the player's card. if 21, black jack, pay 2.5 
 # sum the dealer card
+@player_score = 0
 
+@player_hand.each do |x|
+    @player_score += x.value 
+end
+puts "Your cards:"
+show_card(@player_hand)
+puts "Total Value of your card: #{@player_score}"
+
+if @player_score == 21 #need to add count of card, if 2, go this, else, ignore this
+    puts "BLACKJACK!"
+    endgame #win bet
+    exit
+
+elsif @player_score > 21
+    puts "BUSTED!"
+    endgame #lost bet
+    exit
+end
+
+puts "Would you like to [1] Hit or [2] Stand?"
+player_selection = gets.chomp.to_i
+
+case player_selection
+when 1 then deal(@player_hand)
+    puts "Giving you another card"
+    player_turn
+when 2 then
+    dealer_turn
+end
 
 # Ask player Hit, Stand
 # Player hit, if over 21, but have A, take 10 out of the total score
@@ -47,6 +79,8 @@ def player_turn
 end
 
 def dealer_turn
+    @dealer_score = 0
+
 # reveal Dealer's first card
 # if Stand, Dealer's total is less than 16, hit for dealer
 
@@ -95,6 +129,8 @@ end
 
 def endgame
 #Play again? if yes, go to blackjack_start
+
+puts "End Game Options here to redo game or quit"
 
 
 #if not, return the following information back: Win/Lose Stats, Money Won.
